07252fd310e6bcc7b96bdace61c517e162ca8939
MaiconCanedo <maicon.canedo2013@outlook.com>
2018-07-04 17:09:47 -0300

    Alterando nome da classe "Voto" para "VotoDTO"

---
+++ src/br/gov/tse/urna/dto/VotoDTO.java
@@ -0,0 +1,50 @@
+package br.gov.tse.urna.dto;
+
+import java.sql.Timestamp;
+
+public class VotoDTO {
+	
+	private long idVoto;
+	private EleitorDTO eleitorDTO;
+	private CandidatoDTO candidatoDTO;
+	private Timestamp dataHora;
+	
+	public VotoDTO(long idVoto, EleitorDTO eleitorDTO, CandidatoDTO candidatoDTO, Timestamp dataHora) {
+		this.idVoto = idVoto;
+		this.eleitorDTO = eleitorDTO;
+		this.candidatoDTO = candidatoDTO;
+		this.dataHora = dataHora;
+	}
+	
+	public long getIdVoto() {
+		return idVoto;
+	}
+
+	public void setIdVoto(long idVoto) {
+		this.idVoto = idVoto;
+	}
+
+	public EleitorDTO getEleitorDTO() {
+		return eleitorDTO;
+	}
+
+	public void setEleitorDTO(EleitorDTO eleitorDTO) {
+		this.eleitorDTO = eleitorDTO;
+	}
+
+	public CandidatoDTO getCandidatoDTO() {
+		return candidatoDTO;
+	}
+
+	public void setCandidatoDTO(CandidatoDTO candidatoDTO) {
+		this.candidatoDTO = candidatoDTO;
+	}
+
+	public Timestamp getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(Timestamp dataHora) {
+		this.dataHora = dataHora;
+	}
+}
